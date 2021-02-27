afb63850e86c2bdbfac4689705ea0b4466278c07
tik-217 <tigran.gabulyan.2001@mail.ru>
2021-02-27 11:42:14 +0300

    proba completed

--- src/assets/js/proba.js
+++ src/assets/js/proba.js
@@ -4,21 +4,25 @@ $(function(){
 		infinite: true,
 		slidesPerRow: 1,
 		slidesToShow: 1,
-		// adaptiveHeight: true,
-		variableWidth: true,
 		arrows: false,
+		adaptiveHeight: true,
 		focusOnSelect: false,
 		swipe: false,
 		dots: false,
 		asNavFor: '.proba_content_slider_slides_circle',
+		// responsive: [
+		// {
+		// 	breakpoint: 1200,
+		// 	settings: {
+		// 	}
+		// },
 	});
 
 	$('.proba_content_slider_slides_circle').slick({
-        slidesToShow: 3,
+		slidesToShow: 3,
 		swipe: false,
 		vertical: true,
-        asNavFor: '.proba_content_slider',
-        focusOnSelect: true
-    });
+		asNavFor: '.proba_content_slider',
+		focusOnSelect: true
+	});
 });
-
